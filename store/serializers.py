2d2046aee33d51f2f404249f90b8000d034b5dc2
NipunChamika <nipunchamika11@gmail.com>
2024-05-02 19:37:09 +0530

    feat: Update a CartItem

--- store/serializers.py
+++ store/serializers.py
@@ -105,3 +105,9 @@ class AddCartItemSerializer(serializers.ModelSerializer):
                 cart_id=cart_id, **self.validated_data)
 
         return self.instance
+
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
